734cebccdc234fbb7e770a9e412640e2f757d77f
jiangsuliji <jiangsuliji@gmail.com>
2018-03-27 14:57:36 -0700

    working UCI

--- reUCI.py
+++ reUCI.py
@@ -14,11 +14,12 @@ import numpy as np
 import tensorflow as tf
 
 # Parameters
-learning_rate = 0.1
-batch_size = 100
+#learning_rate = 0.1
+#batch_size = 100
 model_path = "./UCI/nn/NN"
 file_ending = ".ckpt"
 epoch_num = 40 
+num_of_samples = 10
 
 # Network Parameters
 n_hidden_1 = 36 # 1st layer number of features
@@ -73,14 +74,49 @@ def clip_grad(x, clip_value, name=None):
                         name=name,
                         grad=lambda op, g : tf.clip_by_norm(g, clip_value))[0]
 
+# normal sample with mean and var
+def new_weight(mean, var):
+    return var * np.random.randn()+mean
+
+
+# parse input distribution data
+# return distribution, max, min
+distribution = []
+distribution_path = "distribution.txt"
+distribution_max, distribution_min = None, None
+distribution_stage = None
+def read_distribution(path):
+    rtn = []
+    with open(path) as f:
+        lines = f.readlines()
+        for line in lines[1:]:
+            tmp = line.split()
+            rtn.append([float(tmp[1]), float(tmp[2])])
+    return rtn, rtn[-1][0], rtn[0][0]
+
+distribution, distribution_max, distribution_min = read_distribution(distribution_path)
+
+distribution_stage = len(distribution)
+# find distribution parameter 
+# conductance -1 to 1 value,distribution array, max, min of conductance, total stages
+# return mean, stdv
+def check_distribution(c, chi, clo, d, hi, lo, stage):
+    idx = int((c-clo)*stage/(chi-clo))
+    if idx >= stage-1: 
+        m, var = d[-1][0], d[-1][1]
+    else:
+        m, var = (d[idx][0]+d[idx+1][0])/2.0, (d[idx][1]+d[idx+1][1])/2.0
+    m = new_weight(m, var)
+    return (m-lo)/(hi-lo)*(chi-clo) + clo
+
 
 # Create model
 def multilayer_perceptron(x, weights, biases):
     # Hidden layer with RELU activation
-    layer_1 = tf.matmul(x, clip_grad(weights['h1'],1.5))#+biases['b1']
+    layer_1 = tf.matmul(x, weights['h1'])#+biases['b1']
     # layer_1 = tf.tanh(layer_1)
     # Output layer with linear activation
-    out_layer = tf.matmul(layer_1, clip_grad(weights['out'], 1.5))#+biases['out']
+    out_layer = tf.matmul(layer_1, weights['out'])#+biases['out']
 
     #out_layer = tf.layers.dropout(inputs=out_layer,rate=0.5,training=True)
     return out_layer
@@ -101,21 +137,18 @@ pred = multilayer_perceptron(x, weights,biases)
 # Define loss and optimizer
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=y))
 #cost = tf.losses.softmax_cross_entropy(onehot_labels=y, logits=pred)
-optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(cost)
+#optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(cost)
 #optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
 
 # Initialize the variables (i.e. assign their default value)
-init = tf.global_variables_initializer()
+#init = tf.global_variables_initializer()
 
 # 'Saver' op to save and restore all the variables
 saver = tf.train.Saver(max_to_keep=100)
 
 
-
-
-
 # Running first session
-print("Starting 1st session...")
+print("Starting 2nd session...")
 with tf.Session() as sess:
 
     
@@ -128,48 +161,66 @@ with tf.Session() as sess:
 
     print("------staring training eval etc--")
 
-    # Run the initializer
-    sess.run(init)
+    tf.initialize_all_variables().run()
 
-    # Training cycle
-    accuracies = []
+    real_accu = []
+    ideal_accu = []
+
+
     for epoch in range(epoch_num):
         print("--in epoch ", epoch)
         save_path = model_path + str(epoch+1) + file_ending
-        avg_cost = 0.
-        total_batch = int(train_data.shape[0]/batch_size)
+        # Restore model weights from previously saved model
+        saver.restore(sess, save_path)
+        print("Model restored from file: %s" % save_path)
 
-        # Loop over all batches
-        for i in range(total_batch):
-            #batch_x, batch_y = tf.train.batch([images, labels], batch_size=batch_size, capacity=300, enqueue_many=True)
-            #print("--in ",i,"-th batch")
-            training_idx = np.random.randint(train_data.shape[0],size=batch_size)
-            batch_x = train_data[training_idx]
-            batch_y = train_labels[training_idx]
-            
-            # print(training_idx.shape,batch_x.shape, batch_y.shape)
-            # Run optimization op (backprop) and cost op (to get loss value)
-            _, c = sess.run([optimizer, cost], feed_dict={x: batch_x,
-                                                         y: batch_y})
-            # Compute average loss
-            #avg_cost += c / total_batch
-        # Display logs per epoch step
-        #if epoch % display_step == 0:
-        #    print("Epoch:", '%04d' % (epoch+1), "cost=", \
-        #        "{:.9f}".format(avg_cost))
-         
+        # Ideal accuracy test
         # Test model
         correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
         # Calculate accuracy
         accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-        print("Accuracy:", accuracy.eval({x: eval_data, y: eval_labels}))
-        accuracies.append(accuracy.eval({x: eval_data, y: eval_labels})) 
+        
+        print("Ideal Accuracy:", accuracy.eval({x: eval_data, y: eval_labels}))
+        ideal_accu.append(accuracy.eval({x: eval_data, y: eval_labels})) 
 
-        # Save model weights to disk
-        save_path = saver.save(sess, save_path)
-        print("Model saved in file: %s" % save_path)
 
-    print("First Optimization Finished!")
-    print("Final accuracies:", accuracies)
+        # update weight values
+        newh1 = weights['h1'].eval()
+        newout = weights['out'].eval()
+        saveh1 = np.copy(newh1) # deep copy
+        saveout = np.copy(newout)
+        #print(newout)
+        
+        print("weight layer 1 max-min:",newh1.max(),newh1.min())
+        print("weight layer 2 max-min:",newout.max(),newout.min())
 
+        sample_accu = []
+        
+        # loop for # of normal distribution samples
+        for sample in range(num_of_samples):
+            print("ite:",sample)
+            for i in range(0,n_input):
+                for j in range(0,n_hidden_1):
+                    tmp = saveh1[i][j] 
+                    tmp = check_distribution(tmp, 3.7, -3.7,  distribution, distribution_max, distribution_min, distribution_stage)
+                    newh1[i][j] = tmp
+            for i in range(0,n_hidden_1):
+                for j in range(0,n_classes):
+                    tmp = saveout[i][j] 
+                    tmp = check_distribution(tmp, 3.7, -3.7,  distribution, distribution_max, distribution_min, distribution_stage)
+                    newout[i][j] = tmp
+            #print("after-----",newout)
+            weights['h1'].assign(newh1).eval()
+            weights['out'].assign(newout).eval()
+            # Real accuracy test
+            # Test model
+            correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
+            accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+            sample_accu.append(accuracy.eval({x: eval_data, y: eval_labels}))
+        print("Real sample_accu: ",sample_accu)
+        real_accu.append(sum(sample_accu)/(len(sample_accu)))
+
+
+    print("Final ideal accuracies:", ideal_accu)
+    print("Final real accuracies:", real_accu)
 
